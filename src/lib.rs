2f040cfeb97750dcd6242babd048e30b5d0338f0
dalyzhee <dalmasodhiambolevi@gmail.com>
2022-06-03 08:16:09 -0400

    updated-lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,26 @@ use near_sdk::near_bindgen;
 
 #[near_bindgen]
 #[derive(Default, BorshDeserialize, BorshSerialize)]
-pub struct Contract {
-    // SETUP CONTRACT STATE
+pub struct Calculator {
+    num1: u32,
+    num2: u32
 }
 
 #[near_bindgen]
-impl Contract {
-    // ADD CONTRACT METHODS HERE
+impl Calculator {
+    pub fn addition(&self) -> u32{
+        self.num1 + self.num2
+    }
+
+    pub fn substraction(&self) -> u32{
+        self.num1 - self.num2
+    }
+    pub fn multiplication(&self) -> u32{
+        self.num1 * self.num2
+    }
+    pub fn division(&self) -> u32{
+        self.num1 / self.num2
+    }
 }
 
 /*
@@ -23,16 +36,61 @@ impl Contract {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use near_sdk::test_utils::{get_logs, VMContextBuilder};
-    use near_sdk::{testing_env, AccountId};
+    use near_sdk::test_utils::{VMContextBuilder};
+    use near_sdk::{AccountId};
 
     // part of writing unit tests is setting up a mock context
     // provide a `predecessor` here, it'll modify the default context
-    fn get_context(predecessor: AccountId) -> VMContextBuilder {
+    fn get_context(account: AccountId) -> VMContextBuilder {
         let mut builder = VMContextBuilder::new();
-        builder.predecessor_account_id(predecessor);
-        builder
+        builder.signer_account_id(account);
+        return builder;
     }
 
     // TESTS HERE
+    #[test]
+    fn addition(){
+        let user = AccountId::new_unchecked("leviso.testnet".to_string());
+        let _context = get_context(user.clone());
+        let one = Calculator{
+            num1: 12,
+            num2: 10
+        };
+        let add = one.addition();
+        assert_eq!(add, 22);
+    }
+    #[test]
+    fn substraction(){
+        let user = AccountId::new_unchecked("leviso.testnet".to_string());
+        let _context = get_context(user.clone());
+        let one = Calculator{
+            num1: 12,
+            num2: 10
+        };
+        let sub = one.substraction();
+        assert_eq!(sub, 2);
+    }
+    #[test]
+    fn multiplication(){
+        let user = AccountId::new_unchecked("leviso.testnet".to_string());
+        let _context = get_context(user.clone());
+        let one = Calculator{
+            num1: 12,
+            num2: 10
+        };
+        let mult = one.multiplication();
+        assert_eq!(mult, 120);
+    }
+    #[test]
+    fn division(){
+        let user = AccountId::new_unchecked("leviso.testnet".to_string());
+        let _context = get_context(user.clone());
+        let one = Calculator{
+            num1: 120,
+            num2: 10
+        };
+        let div = one.division();
+        assert_eq!(div, 12);
+    }
+
 }
